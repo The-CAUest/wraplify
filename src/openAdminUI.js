2a85849e9a546de2a296848532302b13e34ef01e
DongHyeon - Macbook <hyeoni369@naver.com>
2021-11-18 08:06:40 +0900

    필요한 패키지 받고 간단한 예제 코드 해봄

---
+++ src/openAdminUI.js
@@ -0,0 +1,15 @@
+const inputReader = require("wait-console-input");
+const puppeteer = require("puppeteer");
+
+console.log(123);
+inputReader.readLine("Press a RETURN(ENTER) to continue");
+console.log(12312412);
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.naver.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
