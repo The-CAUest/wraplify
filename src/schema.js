2fddfd5926b518a7721d89510d3c773fb35c129b
j2in <j>
2021-11-19 06:46:22 +0900

    delete file

--- src/schema.js
+++
@@ -1,1 +0,0 @@
-module.exports = {"Note":[{"name":"id","mandatory":true,"type":"ID"},{"name":"name","mandatory":true,"type":"String"},{"name":"completed","mandatory":false,"type":"Boolean"},{"name":"date","mandatory":false,"type":"AWSDate"},{"name":"url","mandatory":false,"type":"AWSURL"},{"name":"age","mandatory":false,"type":"Int"}]}
