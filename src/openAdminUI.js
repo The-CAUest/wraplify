e4e4afb23caac9633036e22d2da0c92924ea1b4d
DongHyeon Kim <hyeoni369@naver.com>
2021-11-19 00:57:00 +0900

    처음에 열어서 admin ui 활성화하는 것까지 완료

--- src/openAdminUI.js
+++ src/openAdminUI.js
@@ -1,18 +1,50 @@
 const inputReader = require("wait-console-input");
 const puppeteer = require("puppeteer");
 const { getProjectInfo } = require("./loader");
+const { sleep } = require("./common");
 
-inputReader.readLine("Press a RETURN(ENTER) After Login!");
 const projectInfo = getProjectInfo();
 console.log(projectInfo);
+
+const projectAdminUIConsole = `https://${projectInfo["amplifyRegion"]}.console.aws.amazon.com/amplify/home?region=${projectInfo["amplifyRegion"]}#/${projectInfo["AmplifyAppId"]}/settings/admin-ui-management`;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.naver.com");
-  const loginBtn = await page.$(".link_login");
-  await loginBtn.click();
-  inputReader.readLine("Press a RETURN(ENTER) to continue");
+  await page.goto(projectAdminUIConsole);
+
+  inputReader.readLine("Press a RETURN(ENTER) After Login!");
+
+  await page.goto(projectAdminUIConsole, { waitUntil: "networkidle2" });
+
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+  let adminUIToggle = await page.$(".awsui-toggle > input");
+  let adminUIToggleValue = await page.evaluate((e) => e.checked, adminUIToggle);
+
+  if (!adminUIToggleValue) {
+    console.log("Enable Admin UI... Please Wait");
+
+    await adminUIToggle.click();
+
+    while (!adminUIToggleValue) {
+      try {
+        await sleep(5000);
+        adminUIToggle = await page.$(".awsui-toggle > input");
+        adminUIToggleValue = await page.evaluate(
+          (e) => e.checked,
+          adminUIToggle
+        );
+      } catch (e) {
+        console.log(e);
+        adminUIToggle = await page.$(".awsui-toggle > input");
+      }
+    }
+
+    console.log("Enable Admin UI Done!");
+  }
+
+  inputReader.readLine("Press a RETURN(ENTER) To Exit!");
 
   await browser.close();
 })();
