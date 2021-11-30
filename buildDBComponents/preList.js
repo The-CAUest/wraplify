5aaee9f71cac70892b7d192569c4d4e739b94d20
nahyun902 <nahyun902@naver.com>
2021-11-30 18:40:27 +0900

    list 필터 수정

--- buildDBComponents/preList.js
+++ buildDBComponents/preList.js
@@ -5,40 +5,23 @@ import ${name} from '../../classes/crudl/${name}'
 import { useEffect, useState } from 'react'
 
 function ${name}List({ filter, showList, style={} }) {
-  const [filteredData, setFilteredData] = useState([])
+  const [data, setData] = useState([])
   
   useEffect(() => {
     ${name}.list${name}().then(data => {
-      let pre_data = {}
-      if(!filter) {
-        pre_data = data
-      } else {
-        const filter_arr = Object.entries(filter)
-        filter_arr.forEach((condition) => {
-          const key = condition[0]
-          const value = condition[1]
-          pre_data = data.filter(elem => elem[key] === value)
-        })
-      }
-      setFilteredData(pre_data)
+      setData(data)
     })
   }, [])
   
-  if (!filteredData) return null
+  if (!data) return null
   
   return (
-    <div
-       className="App"
-       style={{ display: 'flex', justifyContent: 'center', marginTop: 50, ...style }}
-     >
+    <div style={style}>
         <List
-          style={{ marginTop: 16, width:700 }}
           bordered
-          dataSource={filteredData}
+          dataSource={data}
           renderItem={item => (
-            <List.Item
-              style={{textAlign: 'center'}}
-            >
+            <List.Item>
               {showList.map(function (elem) {
                 if (typeof (item[elem]) === 'boolean') {
                   return <Checkbox defaultChecked={item[elem]} disabled>{elem}</Checkbox>
